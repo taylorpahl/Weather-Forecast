9a113019cc82cedfc81969ea5ab969f7297d3d68
Taylor P <taylorpahl97@gmail.com>
2022-12-05 15:47:44 -0600

    Added history local storage

--- script.js
+++ script.js
@@ -12,11 +12,18 @@ var currentWeatherCall = 'weather';
 var directGeoCall = 'direct';
 var iconEl = document.querySelector(".weather-icon");
 
+setVisableHistory();
 
 function getCoordinatesURL() {
   var searchInput = document.getElementById('search-input').value
   console.log(searchInput);
   var result = geoCodingURL + directGeoCall + '?q=' + searchInput + '&limit=1&appid=' + weatherKey;
+  return result;
+}
+
+function getCoordinatesURLHistory(history) {
+  console.log(history);
+  var result = geoCodingURL + directGeoCall + '?q=' + history + '&limit=1&appid=' + weatherKey;
   return result;
 }
 
@@ -102,6 +109,20 @@ function setVisableCurrentWeather(data) {
   document.getElementById('cw-temp').innerHTML ='Temperature: ' + currentWeather.temp + ' °F';
   document.getElementById('cw-wind').innerHTML ='Wind Speed: ' + currentWeather.wind + ' MPH';
   document.getElementById('cw-humidity').innerHTML ='Humidity: ' + currentWeather.humidity + '%';
+  moveHistory();
+  localStorage.setItem('history-1', currentWeather.city);
+  setVisableHistory();
+}
+
+function setVisableHistory() {
+  document.getElementById('history-1').innerHTML = localStorage.getItem('history-1');
+  document.getElementById('history-2').innerHTML = localStorage.getItem('history-2');
+  document.getElementById('history-3').innerHTML = localStorage.getItem('history-3');
+  document.getElementById('history-4').innerHTML = localStorage.getItem('history-4');
+  document.getElementById('history-5').innerHTML = localStorage.getItem('history-5');
+  document.getElementById('history-6').innerHTML = localStorage.getItem('history-6');
+  document.getElementById('history-7').innerHTML = localStorage.getItem('history-7');
+  document.getElementById('history-8').innerHTML = localStorage.getItem('history-8');
 }
 
 function setVisableForecastWeather1(data) {
@@ -179,3 +200,73 @@ function setVisableForecastWeather5(data) {
   document.getElementById('f-wind-5').innerHTML ='Wind Speed: ' + forecastWeather5.wind + ' MPH';
   document.getElementById('f-humidity-5').innerHTML ='Humidity: ' + forecastWeather5.humidity + '%';
 }
+
+function moveHistory() {
+  let newH8 = localStorage.getItem('history-7');
+  localStorage.setItem('history-8', newH8);
+  let newH7 = localStorage.getItem('history-6');
+  localStorage.setItem('history-7', newH7);
+  let newH6 = localStorage.getItem('history-5');
+  localStorage.setItem('history-6', newH6);
+  let newH5 = localStorage.getItem('history-4');
+  localStorage.setItem('history-5', newH5);
+  let newH4 = localStorage.getItem('history-3');
+  localStorage.setItem('history-4', newH4);
+  let newH3 = localStorage.getItem('history-2');
+  localStorage.setItem('history-3', newH3);
+  let newH2 = localStorage.getItem('history-1');
+  localStorage.setItem('history-2', newH2);
+}
+
+$('button').click(function(){
+  let buttonId = $(this).attr('id');
+  var searchCity = localStorage.getItem(buttonId);
+  var coordinateURL = getCoordinatesURLHistory(searchCity);
+  console.log(coordinateURL);
+  fetch(coordinateURL)
+  .then(function (response) {
+    return response.json()
+  })
+  .then(function (data) {
+    var lat = data[0].lat;
+    var lon = data[0].lon;
+    var currentWeatherURL = getCurrentWeatherURL(lat, lon);
+    console.log(currentWeatherURL)
+    console.log(data[0].lat)
+
+    fetch(currentWeatherURL)
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (data) {
+      console.log(data.weather[0].main);
+      console.log(data.main.temp);
+      console.log(data.main.humidity);
+      setVisableCurrentWeather(data);
+
+      var forecastURL  = getForecastURL(lat, lon);
+      console.log(forecastURL);
+      fetch(forecastURL)
+      .then(function (response) {
+        return response.json()
+      })
+      .then(function (data) {
+        // console.log(data.list[0].dt_txt);
+        // console.log(data.list[0].weather[0].icon);
+        // console.log(data.list[0].main.temp);
+        // console.log(data.list[0].main.humidity);
+        // console.log(data.list[0].wind.speed);
+        // console.log(data.list[8].dt_txt);
+        // console.log(data.list[8].weather[0].icon);
+        // console.log(data.list[8].main.temp);
+        // console.log(data.list[8].main.humidity);
+        // console.log(data.list[8].wind.speed);
+        setVisableForecastWeather1(data);
+        setVisableForecastWeather2(data);
+        setVisableForecastWeather3(data);
+        setVisableForecastWeather4(data);
+        setVisableForecastWeather5(data);
+      })
+    }) 
+  })
+})
